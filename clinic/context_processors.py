471451d2b902a069d8a9f779f8b4dc5c3924e9db
Ridwan241 <mohammadridwan241@gmail.com>
2026-05-06 03:59:04 +0600

    bug fixed

--- clinic/context_processors.py
+++ clinic/context_processors.py
@@ -6,7 +6,7 @@ def site_context(request):
     primary_doctor = DoctorProfile.objects.select_related("user").first()
     specialists = get_public_specialists_queryset()[:6]
     return {
-        "site_name": "Shafique's Dental Care",
+        "site_name": "Dr. Shafique's Dental Care",
         "primary_doctor": primary_doctor,
         "home_specialists": specialists,
     }
